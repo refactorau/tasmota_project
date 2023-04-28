ff08e3c69e924500350edf6686686ea3dfadb568
Matt Paine <matt@mattsoftware.com>
2023-04-28 14:17:28 +1000

    Enable custom script to implement a callback on a different topic

--- build_firmware.sh
+++ build_firmware.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 pushd Tasmota-firmware
-git rm -fr release-firmware/*
+git rm -fr development-firmware/*
 git rm -fr firmware/*
 git rm -fr manifest_ext/*
-mkdir -p release-firmware/tasmota32 manifest_ext
+mkdir -p development-firmware/tasmota32 manifest_ext
 popd
 
 pushd Tasmota
 rm -fr Tasmota/build_output
 pio run -e tasmota32-refactor
-cp -a build_output/firmware/tasmota32-* ../Tasmota-firmware/release-firmware/tasmota32
-cp -a build_output/map ../Tasmota-firmware/release-firmware
+cp -a build_output/firmware/tasmota32-* ../Tasmota-firmware/development-firmware/tasmota32
+cp -a build_output/map ../Tasmota-firmware/development-firmware
 popd
 
 pushd Tasmota-firmware
